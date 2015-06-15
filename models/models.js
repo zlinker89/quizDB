bef6d6a523eb2ee124a5a1bf436dccf614e63495
Franklin Ospino <ospi89@hotmail.com>
2015-06-15 17:41:25 -0500

    version 3

--- models/models.js
+++ models/models.js
@@ -1,11 +1,28 @@
 var path = require('path');
 
+// obtenemos la url para postgrade y sqlite
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name     = (url[6] || null);
+var user        = (url[2] || null);
+var pwd         = (url[3] || null);
+var protocol    = (url[1] || null);
+var dialect     = (url[1] || null);
+var port        = (url[5] || null);
+var host        = (url[4] || null);
+var storage     = process.env.DATABASE_STORAGE;
+
 // se carga el ORM
 var Sequelize = require('sequelize');
 
 // se utiliza la base de datos
-var sequelize = new Sequelize(null, null, null,
-    { dialect: "sqlite", storage: "quiz.sqlite" }
+var sequelize = new Sequelize(DB_name, user, pwd,
+    { dialect: dialect,
+      protocol: protocol,
+      port: port,
+      host: host,
+      storage: storage,
+      omitNull: true
+    }
 );
 
 // importamos la definicion de la tabla QUiz en quiz.js
